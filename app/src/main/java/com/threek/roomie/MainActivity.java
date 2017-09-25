0c73d56c1f5e35efbf229178a913dfd809615d3e
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2017-09-25 16:53:53 +0300

    Test 1 commit

--- app/src/main/java/com/threek/roomie/MainActivity.java
+++ app/src/main/java/com/threek/roomie/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action123", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action Test1", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
