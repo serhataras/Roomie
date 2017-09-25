0f173a1c210184df77839c185ced5657f4efa544
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2017-09-25 17:11:25 +0300

    Test 1 commit

--- app/src/main/java/com/threek/roomie/MainActivity.java
+++ app/src/main/java/com/threek/roomie/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-//MainActivityasd
+//MainActivity
 public class MainActivity extends AppCompatActivity {
 
     @Override
