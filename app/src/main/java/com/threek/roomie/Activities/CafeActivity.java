b9cbede5aaa44cdd0adb1a582ae2dc2d8c786e6d
alkislardeniz <alkislardeniz@gmail.com>
2017-12-16 20:38:22 +0300

    Cafe activity fix

--- app/src/main/java/com/threek/roomie/Activities/CafeActivity.java
+++ app/src/main/java/com/threek/roomie/Activities/CafeActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -33,91 +34,68 @@ import src.Game;
 public class CafeActivity extends AppCompatActivity
 {
     private Game game;
-    private TextView foodListTextView;
-    private FoodItem[] menu;
-    private ArrayList<String> data = new ArrayList<>();
+    private CustomListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe);
+
         game = Game.getInstance();
+        adapter = new CustomListAdapter();
 
-        ListView lv = (ListView) findViewById(R.id.foodmenuview);
-        menu = game.getFoodMenu();
-
-        lv.setAdapter(new MyListAdaper(this, R.layout.cafe_food_item, data));
+        ListView listView = (ListView) findViewById(R.id.foodmenuview);
+        listView.setAdapter(adapter);
     }
 
-    private void generateListContent() {
-        for (int i = 0; i < menu.length; i++) {
-            data.add(menu[i].getName()
-                    +"\nHealth: "+menu[i].getHealth()
-                    +"\nPrice: "+menu[i].getPrice());
-        }
-    }
+    // custom adapter for listview
+    private class CustomListAdapter extends BaseAdapter
+    {
+        //public constructor
+        public CustomListAdapter()
+        {}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private class MyListAdaper extends ArrayAdapter<String> {
-        private int layout;
-        private List<String> mObjects;
-        private MyListAdaper(Context context, int resource, List<String> objects) {
-            super(context, resource, objects);
-            mObjects = objects;
-            layout = resource;
+        @Override
+        public int getCount() {
+            return game.getFoodMenu().length; //returns total of items in the list
         }
 
         @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            ViewHolder mainViewholder = null;
-            if(convertView == null) {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView = inflater.inflate(layout, parent, false);
-                ViewHolder viewHolder = new ViewHolder();
-                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
-                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
-                convertView.setTag(viewHolder);
-            }
-            mainViewholder = (ViewHolder) convertView.getTag();
-            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+        public Object getItem(int position) {
+            return game.getFoodMenu()[position]; //returns list item at the specified position
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, final ViewGroup parent)
+        {
+            // inflate the layout for each list row
+            convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.cafe_food_item, parent, false);
+
+            // get current item to be displayed
+            final FoodItem currentItem = (FoodItem) getItem(position);
+
+            // get the TextView for item name and item description
+            //TextView textView = (TextView) convertView.findViewById(R.id.trip_text);
+            TextView itemText = (TextView) convertView.findViewById(R.id.list_item_text);
+            itemText.setText(currentItem.getName());
+
+            Button useButton = (Button) convertView.findViewById(R.id.list_item_btn);
+            useButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    Toast.makeText(getContext(), "Happy Meal!" + position, Toast.LENGTH_SHORT).show();
-                    //Sets the game instance of the cafe selected menu item
-                    ((Cafe)game.getGameEnvironment().getOutdoorEnvironment(OptionType.CAFE_OPTION)).setSelectedFood(menu[position]);
+                public void onClick(View view) {
+                    ((Cafe)game.getGameEnvironment().getOutdoorEnvironment(OptionType.CAFE_OPTION)).setSelectedFood((FoodItem)getItem(position));
+                    finish();
                 }
             });
-            mainViewholder.title.setText(getItem(position));
 
+            // returns the view for the current row
             return convertView;
         }
     }
-    public class ViewHolder {
-        TextView title;
-        Button button;
-    }
 }
-
--- app/src/main/java/src/Game.java
+++ app/src/main/java/src/Game.java
@@ -284,6 +284,7 @@ public class Game
 
     public FoodItem[] getFoodMenu()
     {
+        Log.e("ZAAA", ((Cafe) gameEnvironment.getOutdoorEnvironment(OptionType.CAFE_OPTION)).getMenu()[0].toString());
         return ((Cafe) gameEnvironment.getOutdoorEnvironment(OptionType.CAFE_OPTION)).getMenu();
     }
 
