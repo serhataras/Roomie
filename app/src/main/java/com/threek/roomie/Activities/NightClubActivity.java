2ca4d741110f0b02838d0d9983d648e31dc27e97
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2017-12-16 21:23:10 +0300

    ShakeDetector and NightClub Activity Fix v.4

--- app/src/main/java/com/threek/roomie/Activities/NightClubActivity.java
+++ app/src/main/java/com/threek/roomie/Activities/NightClubActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.threek.roomie.R;
 
+import src.Cafe;
 import src.Enums.OptionType;
+import src.FoodItem;
 import src.Game;
 import src.Hardwares.ShakeDetector;
 import src.NightClub;
@@ -31,7 +35,7 @@ public class NightClubActivity extends AppCompatActivity {
 
     private Game game;
     private boolean success;
-    private int timer=0;
+    private long timer=0;
     public final int MAX_TIME=20;
 
     private final String TAG= NightClubActivity.class.getSimpleName();
@@ -44,57 +48,64 @@ public class NightClubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nightclub);
 
         game = game.getInstance();
-        final NightClub club = (NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION);//Holds the temporary NightClub of the singleton Game object
+        NightClub club = (NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION);//Holds the temporary NightClub of the singleton Game object
 
         countDisplay = (TextView) findViewById(R.id.countView);
         countDisplay.setText("0");
 
         timerDisplay = (TextView) findViewById(R.id.timeView);
-        timerDisplay.setText(MAX_TIME + "");
+        timerDisplay.setText(MAX_TIME + " second to dance!");
 
         clubTag = (TextView) findViewById(R.id.clubName);
         clubTag.setText("Bilkent Night Club");
-
+        club.setShakeAmount(0);
         // ShakeDetector initialization
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager
                 .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mShakeDetector = new ShakeDetector();
-        mShakeDetector.determineLevel(club.getMAX_SHAKE());
+        mShakeDetector.determineLevel(club.getMAX_SHAKE()/10);
         mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
 
             @Override
             public void onShake(final int count) {
-				/*
+
+                /*
 				 * The following method, "handleShakeEvent(count):" is a stub //
 				 * method you would use to setup whatever you want done once the
 				 * device has been shook.
 				 */
                 //TODO time interval
                 Handler handler = new Handler();
-
                 new CountDownTimer(MAX_TIME*1000, 1000) {
 
                     public void onTick(long millisUntilFinished) {
-                        timerDisplay.setText("seconds remaining: " + millisUntilFinished / 1000 + " dance on!");
-                        countDisplay.setText(count);
+                        timer=millisUntilFinished / 1000;
+                        updateView();
                         //if threshold passed, dont need to dance anymore
-                        if(mShakeDetector.handleShakeEvent()){
-                            ((NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION)).setShakeAmount(count);
+                        if(mShakeDetector.get_mShakeCount()>mShakeDetector.getShakeAmount()){
+                            ((NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION)).setShakeAmount(mShakeDetector.get_mShakeCount());
                             finish();
                         }
                     }
                     public void onFinish() {
                         timerDisplay.setText("done!");
                         //the game instance updated
-                        ((NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION)).setShakeAmount(count);
+                        ((NightClub) game.getGameEnvironment().getOutdoorEnvironment(OptionType.NIGHT_CLUB_OPTION)).setShakeAmount(mShakeDetector.get_mShakeCount());
                         finish();
                     }
                 }.start();
 
+
             }
         });
-
+        Button exitButton = (Button)findViewById(R.id.clubExit);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
     @Override
     public void onResume() {
@@ -102,7 +113,13 @@ public class NightClubActivity extends AppCompatActivity {
         // Add the following line to register the Session Manager Listener onResume
         mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
     }
-
+    public void updateView(){
+        if(mShakeDetector.get_mShakeCount() > 0) {
+            countDisplay.setText(String.valueOf(mShakeDetector.get_mShakeCount()));
+        }
+        else
+            countDisplay.setText("0");
+    }
     @Override
     public void onPause() {
         // Add the following line to unregister the Sensor Manager onPause
